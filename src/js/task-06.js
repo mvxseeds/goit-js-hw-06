12c80480b5f7525a75998ddde89c48c4d8c397ec
Max Semena <me.semena@gmail.com>
2022-12-03 13:40:05 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,8 +3,13 @@ const input = document.getElementById('validation-input');
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    event.currentTarget.value.length < event.currentTarget.dataset.length
-    ? event.currentTarget.classList.add('invalid')
-    : event.currentTarget.classList.add('valid');
+    if (event.currentTarget.value.length < event.currentTarget.dataset.length) {
+      event.currentTarget.classList.add('invalid')
+    } else {
+      if (event.currentTarget.classList.contains('invalid')) {
+        event.currentTarget.classList.remove('invalid');
+      }
+      event.currentTarget.classList.add('valid');
+    }
 }
   
